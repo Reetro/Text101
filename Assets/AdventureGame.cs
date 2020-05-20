9f2bfe31414a74f60fb50ce0d531321c06aa7dfc
Reetro <thegamerboy7@gmail.com>
2020-05-19 22:45:09 -0600

    Added Files

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+  // SerializeField exposes this to the details panel
+  [SerializeField] Text TextComponent;
+  [SerializeField] State StartingState;
+
+  State CurrentState;
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+    CurrentState = StartingState;
+    TextComponent.text = CurrentState.GetStateStory();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ManageState();
+  }
+
+  private void ManageState()
+  {
+    var NextStates = CurrentState.GetNextStates();
+
+    for (int index = 0; index < NextStates.Length; index++)
+    {
+      if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+      {
+        CurrentState = NextStates[index];
+      }
+    }
+
+    TextComponent.text = CurrentState.GetStateStory();
+  }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adds item to game object menu
+[CreateAssetMenu(menuName = ("State"))]
+public class State : ScriptableObject
+{
+  // The text are field will set the size of the text field within the details panel 10 min lines shown and 14 is point it will start scrolling
+  [TextArea(10, 14)] [SerializeField] string storyText;
+
+  // Array of possible next states
+  [SerializeField] State[] NextStates;
+
+  public string GetStateStory()
+  {
+    return storyText;
+  }
+
+  public State[] GetNextStates()
+  {
+    return NextStates;
+  }
+}
